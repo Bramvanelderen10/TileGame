86581b40b85f875cfda37c09926220b75b3a61aa
Jordi van den Nouweland <87757>
2016-05-19 14:46:23 +0200

    main menu commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public Transform mainMenu, optionMenu;
+
+    public void LoadScene(string name){
+        SceneManager.LoadScene(name);
+    }
+}
