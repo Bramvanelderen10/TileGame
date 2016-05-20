a98685ad97d622cd96c0e0cbb529fec2640f2e99
Bram van Elderen <bramvanelderen1994@gmail.com>
2016-05-20 12:58:25 +0200

    Background scaling added

---
+++ Assets/Scripts/ScaleToBackground.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScaleToBackground : MonoBehaviour {
+
+    private Sprite sprite;
+
+	// Use this for initialization
+	void Start () {
+        sprite = this.gameObject.GetComponent<SpriteRenderer>().sprite;
+
+        Camera cam = Camera.main;
+
+        float height = (float)Camera.main.orthographicSize * 2.0f;
+        float width = height * Screen.width / Screen.height;
+        Vector2 dimensions = new Vector2(width, height);
+        ScaleSpriteToTile(this.gameObject, dimensions);
+
+    }
+
+    void ScaleSpriteToTile(GameObject sprite, Vector2 dimensions)
+    {
+        Sprite sp = sprite.GetComponent<SpriteRenderer>().sprite;
+
+        Vector3 spriteBounds = SpriteLocalToWorld(sp);
+        Vector3 localScale = sprite.transform.localScale;
+
+        localScale.x = (localScale.x * dimensions.x) / spriteBounds.x;
+        localScale.y = (localScale.y * dimensions.y) / spriteBounds.y * -1;
+        sprite.transform.localScale = localScale;
+    }
+
+    //Returns the width and height of a sprite in units
+    Vector3 SpriteLocalToWorld(Sprite sp)
+    {
+        Vector3 pos = transform.position;
+        Vector3[] array = new Vector3[2];
+
+        Vector3 result = new Vector3(0, 0, 0);
+        //top left
+        array[0] = pos + sp.bounds.min;
+        // Bottom right
+        array[1] = pos + sp.bounds.max;
+
+        result.x = array[1].x - array[0].x;
+        result.y = array[0].y - array[1].y;
+
+        return result;
+    }
+}
